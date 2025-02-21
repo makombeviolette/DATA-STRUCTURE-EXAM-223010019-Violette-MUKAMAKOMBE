35a7fa2c5c3fc07dd79f471aef4c54ccbb99356e
MUKAMAKOMBE Violette <violettemukamakombe@gmail.com>
2025-02-21 10:15:09 +0200

    Add files via upload

---
+++ 4. on Operators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+      cout << "OPERATORS IN C++"<<endl<<endl;
+      int a = 12, b = 4;
+      cout << "Sum: " << a + b << endl;
+      cout << "Difference: " << a - b << endl;
+      cout << "Product: " << a * b << endl;
+      cout << "Quotient: " << a / b << endl;
+      cout << "Remainder: " << a % b << endl;
+
+}
